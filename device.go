82895c65146ffe9a87ef48fceb82094d5df70760
Maksym Pavlenko <pavlenko.maksym@gmail.com>
2018-01-17 16:27:01 -0800

    Add Device Queries stubs

---
+++ device.go
@@ -0,0 +1,349 @@
+package nvml
+
+func (w wrapper) DeviceClearCpuAffinity() {
+
+}
+
+func (w wrapper) DeviceGetAPIRestriction() {
+
+}
+
+func (w wrapper) DeviceGetApplicationsClock() {
+
+}
+
+func (w wrapper) DeviceGetAutoBoostedClocksEnabled() {
+
+}
+
+func (w wrapper) DeviceGetBAR1MemoryInfo() {
+
+}
+
+func (w wrapper) DeviceGetBoardId() {
+
+}
+
+func (w wrapper) DeviceGetBoardPartNumber() {
+
+}
+
+func (w wrapper) DeviceGetBrand() {
+
+}
+
+func (w wrapper) DeviceGetBridgeChipInfo() {
+
+}
+
+func (w wrapper) DeviceGetClock() {
+
+}
+
+func (w wrapper) DeviceGetClockInfo() {
+
+}
+
+func (w wrapper) DeviceGetComputeMode() {
+
+}
+
+func (w wrapper) DeviceGetComputeRunningProcesses() {
+
+}
+
+func (w wrapper) DeviceGetCount() {
+
+}
+
+func (w wrapper) DeviceGetCpuAffinity() {
+
+}
+
+func (w wrapper) DeviceGetCudaComputeCapability() {
+
+}
+
+func (w wrapper) DeviceGetCurrPcieLinkGeneration() {
+
+}
+
+func (w wrapper) DeviceGetCurrPcieLinkWidth() {
+
+}
+
+func (w wrapper) DeviceGetCurrentClocksThrottleReasons() {
+
+}
+
+func (w wrapper) DeviceGetDecoderUtilization() {
+
+}
+
+func (w wrapper) DeviceGetDefaultApplicationsClock() {
+
+}
+
+func (w wrapper) DeviceGetDetailedEccErrors() {
+
+}
+
+func (w wrapper) DeviceGetDisplayActive() {
+
+}
+
+func (w wrapper) DeviceGetDisplayMode() {
+
+}
+
+func (w wrapper) DeviceGetDriverModel() {
+
+}
+
+func (w wrapper) DeviceGetEccMode() {
+
+}
+
+func (w wrapper) DeviceGetEncoderCapacity() {
+
+}
+
+func (w wrapper) DeviceGetEncoderSessions() {
+
+}
+
+func (w wrapper) DeviceGetEncoderStats() {
+
+}
+
+func (w wrapper) DeviceGetEncoderUtilization() {
+
+}
+
+func (w wrapper) DeviceGetEnforcedPowerLimit() {
+
+}
+
+func (w wrapper) DeviceGetFanSpeed() {
+
+}
+
+func (w wrapper) DeviceGetGpuOperationMode() {
+
+}
+
+func (w wrapper) DeviceGetGraphicsRunningProcesses() {
+
+}
+
+func (w wrapper) DeviceGetHandleByIndex() {
+
+}
+
+func (w wrapper) DeviceGetHandleByPciBusId() {
+
+}
+
+func (w wrapper) DeviceGetHandleBySerial() {
+
+}
+
+func (w wrapper) DeviceGetHandleByUUID() {
+
+}
+
+func (w wrapper) DeviceGetIndex() {
+
+}
+
+func (w wrapper) DeviceGetInforomConfigurationChecksum() {
+
+}
+
+func (w wrapper) DeviceGetInforomImageVersion() {
+
+}
+
+func (w wrapper) DeviceGetInforomVersion() {
+
+}
+
+func (w wrapper) DeviceGetMaxClockInfo() {
+
+}
+
+func (w wrapper) DeviceGetMaxCustomerBoostClock() {
+
+}
+
+func (w wrapper) DeviceGetMaxPcieLinkGeneration() {
+
+}
+
+func (w wrapper) DeviceGetMaxPcieLinkWidth() {
+
+}
+
+func (w wrapper) DeviceGetMemoryErrorCounter() {
+
+}
+
+func (w wrapper) DeviceGetMemoryInfo() {
+
+}
+
+func (w wrapper) DeviceGetMinorNumber() {
+
+}
+
+func (w wrapper) DeviceGetMultiGpuBoard() {
+
+}
+
+func (w wrapper) DeviceGetName() {
+
+}
+
+func (w wrapper) DeviceGetP2PStatus() {
+
+}
+
+func (w wrapper) DeviceGetPciInfo() {
+
+}
+
+func (w wrapper) DeviceGetPcieReplayCounter() {
+
+}
+
+func (w wrapper) DeviceGetPcieThroughput() {
+
+}
+
+func (w wrapper) DeviceGetPerformanceState() {
+
+}
+
+func (w wrapper) DeviceGetPersistenceMode() {
+
+}
+
+func (w wrapper) DeviceGetPowerManagementDefaultLimit() {
+
+}
+
+func (w wrapper) DeviceGetPowerManagementLimit() {
+
+}
+
+func (w wrapper) DeviceGetPowerManagementLimitConstraints() {
+
+}
+
+func (w wrapper) DeviceGetPowerManagementMode() {
+
+}
+
+func (w wrapper) DeviceGetPowerState() {
+
+}
+
+func (w wrapper) DeviceGetPowerUsage() {
+
+}
+
+func (w wrapper) DeviceGetRetiredPages() {
+
+}
+
+func (w wrapper) DeviceGetRetiredPagesPendingStatus() {
+
+}
+
+func (w wrapper) DeviceGetSamples() {
+
+}
+
+func (w wrapper) DeviceGetSerial() {
+
+}
+
+func (w wrapper) DeviceGetSupportedClocksThrottleReasons() {
+
+}
+
+func (w wrapper) DeviceGetSupportedGraphicsClocks() {
+
+}
+
+func (w wrapper) DeviceGetSupportedMemoryClocks() {
+
+}
+
+func (w wrapper) DeviceGetTemperature() {
+
+}
+
+func (w wrapper) DeviceGetTemperatureThreshold() {
+
+}
+
+func (w wrapper) DeviceGetTopologyCommonAncestor() {
+
+}
+
+func (w wrapper) DeviceGetTopologyNearestGpus() {
+
+}
+
+func (w wrapper) DeviceGetTotalEccErrors() {
+
+}
+
+func (w wrapper) DeviceGetTotalEnergyConsumption() {
+
+}
+
+func (w wrapper) DeviceGetUUID() {
+
+}
+
+func (w wrapper) DeviceGetUtilizationRates() {
+
+}
+
+func (w wrapper) DeviceGetVbiosVersion() {
+
+}
+
+func (w wrapper) DeviceGetViolationStatus() {
+
+}
+
+func (w wrapper) DeviceOnSameBoard() {
+
+}
+
+func (w wrapper) DeviceResetApplicationsClocks() {
+
+}
+
+func (w wrapper) DeviceSetAutoBoostedClocksEnabled() {
+
+}
+
+func (w wrapper) DeviceSetCpuAffinity() {
+
+}
+
+func (w wrapper) DeviceSetDefaultAutoBoostedClocksEnabled() {
+
+}
+
+func (w wrapper) DeviceValidateInforom() {
+
+}
+
+func (w wrapper) SystemGetTopologyGpuSetfunc() {
+
+}
---
+++ device_test.go
@@ -0,0 +1,351 @@
+package nvml
+
+import "testing"
+
+func TestDeviceClearCpuAffinity(t *testing.T) {
+
+}
+
+func TestDeviceGetAPIRestriction(t *testing.T) {
+
+}
+
+func TestDeviceGetApplicationsClock(t *testing.T) {
+
+}
+
+func TestDeviceGetAutoBoostedClocksEnabled(t *testing.T) {
+
+}
+
+func TestDeviceGetBAR1MemoryInfo(t *testing.T) {
+
+}
+
+func TestDeviceGetBoardId(t *testing.T) {
+
+}
+
+func TestDeviceGetBoardPartNumber(t *testing.T) {
+
+}
+
+func TestDeviceGetBrand(t *testing.T) {
+
+}
+
+func TestDeviceGetBridgeChipInfo(t *testing.T) {
+
+}
+
+func TestDeviceGetClock(t *testing.T) {
+
+}
+
+func TestDeviceGetClockInfo(t *testing.T) {
+
+}
+
+func TestDeviceGetComputeMode(t *testing.T) {
+
+}
+
+func TestDeviceGetComputeRunningProcesses(t *testing.T) {
+
+}
+
+func TestDeviceGetCount(t *testing.T) {
+
+}
+
+func TestDeviceGetCpuAffinity(t *testing.T) {
+
+}
+
+func TestDeviceGetCudaComputeCapability(t *testing.T) {
+
+}
+
+func TestDeviceGetCurrPcieLinkGeneration(t *testing.T) {
+
+}
+
+func TestDeviceGetCurrPcieLinkWidth(t *testing.T) {
+
+}
+
+func TestDeviceGetCurrentClocksThrottleReasons(t *testing.T) {
+
+}
+
+func TestDeviceGetDecoderUtilization(t *testing.T) {
+
+}
+
+func TestDeviceGetDefaultApplicationsClock(t *testing.T) {
+
+}
+
+func TestDeviceGetDetailedEccErrors(t *testing.T) {
+
+}
+
+func TestDeviceGetDisplayActive(t *testing.T) {
+
+}
+
+func TestDeviceGetDisplayMode(t *testing.T) {
+
+}
+
+func TestDeviceGetDriverModel(t *testing.T) {
+
+}
+
+func TestDeviceGetEccMode(t *testing.T) {
+
+}
+
+func TestDeviceGetEncoderCapacity(t *testing.T) {
+
+}
+
+func TestDeviceGetEncoderSessions(t *testing.T) {
+
+}
+
+func TestDeviceGetEncoderStats(t *testing.T) {
+
+}
+
+func TestDeviceGetEncoderUtilization(t *testing.T) {
+
+}
+
+func TestDeviceGetEnforcedPowerLimit(t *testing.T) {
+
+}
+
+func TestDeviceGetFanSpeed(t *testing.T) {
+
+}
+
+func TestDeviceGetGpuOperationMode(t *testing.T) {
+
+}
+
+func TestDeviceGetGraphicsRunningProcesses(t *testing.T) {
+
+}
+
+func TestDeviceGetHandleByIndex(t *testing.T) {
+
+}
+
+func TestDeviceGetHandleByPciBusId(t *testing.T) {
+
+}
+
+func TestDeviceGetHandleBySerial(t *testing.T) {
+
+}
+
+func TestDeviceGetHandleByUUID(t *testing.T) {
+
+}
+
+func TestDeviceGetIndex(t *testing.T) {
+
+}
+
+func TestDeviceGetInforomConfigurationChecksum(t *testing.T) {
+
+}
+
+func TestDeviceGetInforomImageVersion(t *testing.T) {
+
+}
+
+func TestDeviceGetInforomVersion(t *testing.T) {
+
+}
+
+func TestDeviceGetMaxClockInfo(t *testing.T) {
+
+}
+
+func TestDeviceGetMaxCustomerBoostClock(t *testing.T) {
+
+}
+
+func TestDeviceGetMaxPcieLinkGeneration(t *testing.T) {
+
+}
+
+func TestDeviceGetMaxPcieLinkWidth(t *testing.T) {
+
+}
+
+func TestDeviceGetMemoryErrorCounter(t *testing.T) {
+
+}
+
+func TestDeviceGetMemoryInfo(t *testing.T) {
+
+}
+
+func TestDeviceGetMinorNumber(t *testing.T) {
+
+}
+
+func TestDeviceGetMultiGpuBoard(t *testing.T) {
+
+}
+
+func TestDeviceGetName(t *testing.T) {
+
+}
+
+func TestDeviceGetP2PStatus(t *testing.T) {
+
+}
+
+func TestDeviceGetPciInfo(t *testing.T) {
+
+}
+
+func TestDeviceGetPcieReplayCounter(t *testing.T) {
+
+}
+
+func TestDeviceGetPcieThroughput(t *testing.T) {
+
+}
+
+func TestDeviceGetPerformanceState(t *testing.T) {
+
+}
+
+func TestDeviceGetPersistenceMode(t *testing.T) {
+
+}
+
+func TestDeviceGetPowerManagementDefaultLimit(t *testing.T) {
+
+}
+
+func TestDeviceGetPowerManagementLimit(t *testing.T) {
+
+}
+
+func TestDeviceGetPowerManagementLimitConstraints(t *testing.T) {
+
+}
+
+func TestDeviceGetPowerManagementMode(t *testing.T) {
+
+}
+
+func TestDeviceGetPowerState(t *testing.T) {
+
+}
+
+func TestDeviceGetPowerUsage(t *testing.T) {
+
+}
+
+func TestDeviceGetRetiredPages(t *testing.T) {
+
+}
+
+func TestDeviceGetRetiredPagesPendingStatus(t *testing.T) {
+
+}
+
+func TestDeviceGetSamples(t *testing.T) {
+
+}
+
+func TestDeviceGetSerial(t *testing.T) {
+
+}
+
+func TestDeviceGetSupportedClocksThrottleReasons(t *testing.T) {
+
+}
+
+func TestDeviceGetSupportedGraphicsClocks(t *testing.T) {
+
+}
+
+func TestDeviceGetSupportedMemoryClocks(t *testing.T) {
+
+}
+
+func TestDeviceGetTemperature(t *testing.T) {
+
+}
+
+func TestDeviceGetTemperatureThreshold(t *testing.T) {
+
+}
+
+func TestDeviceGetTopologyCommonAncestor(t *testing.T) {
+
+}
+
+func TestDeviceGetTopologyNearestGpus(t *testing.T) {
+
+}
+
+func TestDeviceGetTotalEccErrors(t *testing.T) {
+
+}
+
+func TestDeviceGetTotalEnergyConsumption(t *testing.T) {
+
+}
+
+func TestDeviceGetUUID(t *testing.T) {
+
+}
+
+func TestDeviceGetUtilizationRates(t *testing.T) {
+
+}
+
+func TestDeviceGetVbiosVersion(t *testing.T) {
+
+}
+
+func TestDeviceGetViolationStatus(t *testing.T) {
+
+}
+
+func TestDeviceOnSameBoard(t *testing.T) {
+
+}
+
+func TestDeviceResetApplicationsClocks(t *testing.T) {
+
+}
+
+func TestDeviceSetAutoBoostedClocksEnabled(t *testing.T) {
+
+}
+
+func TestDeviceSetCpuAffinity(t *testing.T) {
+
+}
+
+func TestDeviceSetDefaultAutoBoostedClocksEnabled(t *testing.T) {
+
+}
+
+func TestDeviceValidateInforom(t *testing.T) {
+
+}
+
+func TestSystemGetTopologyGpuSetfunc(t *testing.T) {
+
+}
